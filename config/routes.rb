96039c062eca8af54d89f09389065a0d041aa40a
ryo <ba068082@gmail.com>
2017-06-27 12:35:57 +0900

    調整前コミット

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :patients
-  resources :histories
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+    # トップページ
+    root 'static__pages#home'
+
+    # 患者登録
+    resources :patients
+
+    # 履歴登録
+    resources :histories
 end
