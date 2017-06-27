5d4c317dba8a85eb704884106d65efef89f7c3a1
ryo <ba068082@gmail.com>
2017-06-27 21:55:31 +0900

    患者検索の実装完了。詳細情報へのリンクもよし

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -4,6 +4,11 @@ class PatientsController < ApplicationController
         @patients = Patient.page(params[:page])
     end
 
+    # 患者名簿検索
+    def search
+        gon.patients = Patient.all
+    end
+
     # 患者情報追加
     def new
         @patient = Patient.new
@@ -24,17 +29,33 @@ class PatientsController < ApplicationController
         @patient = Patient.find(params[:id])
     end
 
-    # 患者情報個別
+    # 患者情報修正
     def edit
         @patient = Patient.find(params[:id])
+    end
+
+    # 患者情報の修正をDBへ反映
+    def update
+        @patient = Patient.find(params[:id])
+        if @patient.update_attributes(patient_params)
+            redirect_to @patient
+        else
+            render 'edit'
+        end
+    end
+
+    # 患者情報の削除
+    def destroy
+        Patient.find(params[:id]).destroy
+        redirect_to patients_url
     end
 
 
     # フォーム受け取り要素の選別
     private
-        def patient_params
-            params.require(:patient).permit(:name, :kana, :sex, :birthdate, :phone,
-                                            :postCode, :address, :reason, :experience,
-                                            :firstDay, :memo)
-        end
+    def patient_params
+        params.require(:patient).permit(:name, :kana, :sex, :birthdate, :phone,
+                                        :postCode, :address, :reason, :experience,
+                                        :firstDay, :memo)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
     # トップページ
     root 'static__pages#home'
 
+    # 患者名簿検索
+    get '/patients/search', to: 'patients#search', as: 'search'
+
     # 患者登録
     resources :patients
 
