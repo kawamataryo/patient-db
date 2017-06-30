b5c11911f8f09188500fbb26049e6c0bb8e734f8
ryo <ba068082@gmail.com>
2017-06-30 12:39:12 +0900

    履歴の保存ロジック完了

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -1,36 +1,45 @@
 class HistoriesController < ApplicationController
+
+    # 履歴の追加
     def new
         gon.patients = Patient.all
     end
 
+    # 履歴のDB記録
     def create
         # postの取得
         @history_date = params[:history_date]
         @patient_id = params[:patient_id]
         @sales = params[:sales]
 
-        # 分割取得
+        # DB記録
         count = 0
-        @histories = []
-        for patient in @patient_id do
-            unless patient == ''
-                @histories.push({ history_date: @history_date, patient_id: patient, sales: @sales["#{count}"] })
+        for id in @patient_id do
+            # idが空行でなかったら保存へ
+            unless id == ''
+                @history = History.new
+                # データ挿入
+                @history.historyDate = @history_date
+                @history.patient_id = id
+                @history.patientName = Patient.find(id).name
+                @history.sales = @sales["#{count}"].to_i
+                # 保存
+                @history.save
             end
             count += 1
         end
     end
 
-    def show
+    # 履歴の削除
+    def destroy
+        History.find(params[:id]).destroy
+        redirect_to histories_url
     end
 
+    # 履歴一覧
     def index
-        @histories = History.all
+        # 日付順で並び替えて出力
+        @histories = History.order("historyDate desc").page(params[:page])
     end
 
-    private
-        def history_params
-            history_date = params["history_date"]
-            patient_id = params["patient_id"]
-            sales = params["sales"]
-        end
 end
