9b455e6edc649973e1b503cf3b1b6b62879b33fa
ryo <ba068082@gmail.com>
2017-07-08 22:26:29 +0900

    ヒストリーバリデート試行錯誤

--- app/models/history.rb
+++ app/models/history.rb
@@ -1,5 +1,10 @@
 class History < ApplicationRecord
     # patientとの関連付け
     belongs_to :patient
+    # validate
+    # 日付を必須
+    validates :history_date, presence: true
+    # patientDBにあるかどう確認
+    validates :patient_id, :inclusion => {:in => proc{Patient.pluck(:id)}}
 
 end
