1a98ea3d2155817371edfe905858f9085cdea366
ryo <ba068082@gmail.com>
2017-08-19 15:52:50 +0900

    patient_idを新たにカラ��に追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170819062705) do
+ActiveRecord::Schema.define(version: 20170819063502) do
 
   create_table "histories", force: :cascade do |t|
     t.date "history_date"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20170819062705) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "deleted_at"
+    t.integer "patient_id"
     t.index ["deleted_at"], name: "index_patients_on_deleted_at"
   end
 
