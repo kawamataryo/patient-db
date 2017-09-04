3f4472f73971f8839eadd3a005d320cfab577b2c
ryo <ba068082@gmail.com>
2017-09-04 09:59:39 +0900

    formating downloads index view

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -6,8 +6,17 @@ class DownloadsController < ApplicationController
 
     #downloadcsv
     def yearSales
+        #yearの年の売上集計結果を出力
         dw_year = params[:year]
         @day_sales = History.where("history_date like '" + dw_year + "%'").group_by_day(:history_date, format: "%Y/%m/%d").sum(:sales)
+
+        #file名を編集
+        respond_to do |format|
+          format.csv do
+              send_data render_to_string, filename: dw_year + "-sales.csv", type: :csv
+          end
+        end
+
     end
 
 end
