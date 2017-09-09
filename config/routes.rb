774c59e14e157f5c943e681b7759816cb707800b
ryo <ba068082@gmail.com>
2017-09-09 16:50:37 +0900

    routes URL auth message bag modified!!

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
 
+
     # downloads page
     get 'downloads/index'
 
     # downloads sales
-    #get 'downloads/yearSales/:year' => 'downloads#yearSales'
     get 'downloads/yearSales'
 
     #グラフ
@@ -13,8 +13,16 @@ Rails.application.routes.draw do
     # 認証
     devise_for :users
 
-    # トップページ
-    root 'static__pages#home'
+    # 認証されていないユーザーのホーム
+    unauthenticated do
+        as :user do
+            root to: 'devise/sessions#new'
+        end
+    end
+
+    # 認証されているユーザーのホーム
+    root to: 'static_pages#home'
+
 
     # 患者名簿検索
     get '/patients/search', to: 'patients#search', as: 'search'
