8161d1401e941b5e9ceb4caa09bff4944b20601e
ryo <ba068082@gmail.com>
2017-09-13 14:10:06 +0900

    add symptom to patient migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170913043822) do
+ActiveRecord::Schema.define(version: 20170913050954) do
 
   create_table "histories", force: :cascade do |t|
     t.date "history_date"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20170913043822) do
     t.datetime "deleted_at"
     t.integer "patient_id"
     t.string "email"
+    t.string "symptom"
     t.index ["deleted_at"], name: "index_patients_on_deleted_at"
   end
 
