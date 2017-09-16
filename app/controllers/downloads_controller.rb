9df049eab4373815db893a0b60eeb7ef898b0df8
ryo <ba068082@gmail.com>
2017-09-16 14:54:12 +0900

    downloads

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -8,7 +8,7 @@ class DownloadsController < ApplicationController
     def yearSales
         #yearの年の売上集計結果を出力
         dw_year = params[:year]
-        @day_sales = History.where("history_date like ?", "%#{dw_year}%").group_by_day(:history_date, format: "%Y/%m/%d").sum(:sales)
+        @day_sales = History.where("history_date like ?", dw_year + "%").group_by_day(:history_date, format: "%Y/%m/%d").sum(:sales)
 
         #file名を編集
         respond_to do |format|
