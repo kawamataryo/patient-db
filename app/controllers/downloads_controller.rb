4a04ec17c1b0da2a8a09fb3364c263096abe7f61
ryo <ba068082@gmail.com>
2017-09-16 15:19:09 +0900

    modi download between method

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -1,14 +1,20 @@
 class DownloadsController < ApplicationController
 
+    require 'time'
+
     #downloadpage
     def index
     end
 
     #downloadcsv
     def yearSales
-        #yearの年の売上集計結果を出力
+        # 受けった年をparseする
         dw_year = params[:year]
-        @day_sales = History.where("history_date like ?", dw_year + "%").group_by_day(:history_date, format: "%Y/%m/%d").sum(:sales)
+        first_day = Time.parse(dw_year + "-01-01")
+        last_day = Time.parse(dw_year + "-12-31")
+
+        #yearの年の売上集計結果を出力
+        @day_sales = History.where(history_date: first_day..last_day).group_by_day(:history_date, format: "%Y/%m/%d").sum(:sales)
 
         #file名を編集
         respond_to do |format|
