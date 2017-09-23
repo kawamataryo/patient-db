066f4f6fa8e4c051c583e58ac6a4c7c1cbe873ef
ryo <ba068082@gmail.com>
2017-09-23 23:05:59 +0900

    add favicon

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,10 @@ module Pv3
         # Application configuration should go into files in config/initializers
         # -- all .rb files in that directory are automatically loaded.
 
+        # autoload
+        config.enable_dependency_loading = true
+        config.autoload_paths << Rails.root.join('lib')
+
         # アクティブレコード拡張
         config.autoload_paths += %W(
         #{config.root}/lib
