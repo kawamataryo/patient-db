c79f26d25ff66b4da59e0daf3cb704e6e6a72896
ryo <ba068082@gmail.com>
2017-09-29 21:46:59 +0900

    reset db

--- db/migrate/20170928020234_add_arm_to_patient.rb
+++
@@ -1,6 +0,0 @@
-class AddArmToPatient < ActiveRecord::Migration[5.1]
-  def change
-    add_column :patients, :arm, :boolean, default: false, null: false
-    remove_column :patients, :sports, :boolean
-  end
-end
