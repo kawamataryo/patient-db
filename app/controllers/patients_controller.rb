6e95f739ab644b1c70c8aa04de7db8fc57ee8fd5
ryo <ba068082@gmail.com>
2017-09-30 12:21:14 +0900

    add sex to patient show tb

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -69,8 +69,8 @@ class PatientsController < ApplicationController
             param = params.require(:patient).permit(:patient_id, :name, :kana, :sex, :birthdate, :phone,
                                             :post_code, :address, :reason, :experience, :email,
                                             :firstday, :memo, symptom: [])
-            #配列要素を文字列に変換
-            param[:symptom] = param[:symptom].join(', ')
+            #配列要素を文字列に変換(入力されているときのみ)
+            param[:symptom] = param[:symptom].join(', ') if param[:symptom]
 
             return param
         end
