f663db9d8ee1a5fae50ebeeedcf6380af8af1d93
ryo <ba068082@gmail.com>
2017-10-10 23:43:57 +0900

    modelのvalidate完成。factoryの利用もOK

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -5,6 +5,7 @@ class Patient < ApplicationRecord
     has_many :history
     #validate
     validates :name, presence: true #名前を必須
-    validates :patient_id, presence: true #IDを必須
-    validates :patient_id, uniqueness: true #IDを一意に
+    validates :patient_id, presence: true #IDを必須、
+    validates :patient_id, uniqueness: true #一意
+    validates :firstday, presence: true #初診日を必須
 end
---
+++ spec/factories/patients.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :patient do
+    name 'ryo'
+    patient_id 1
+    firstday Date.today
+  end
+end
