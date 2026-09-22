b60f2f3059af6b6923ec347265a0bdad42fe8f5d
kawamataryo <ba068082@gmail.com>
2026-09-22 19:40:15 +0900

    Remove exposed credentials

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -6,7 +6,7 @@ Devise.setup do |config|
   # confirmation, reset password and unlock tokens in the database.
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
-  # config.secret_key = 'REMOVED_CREDENTIAL'
+  # Use Rails secret_key_base; production reads SECRET_KEY_BASE from the environment.
 
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
